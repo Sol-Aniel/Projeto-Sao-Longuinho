1340aaf9fac808464f62090554f907a9bd3b7c47
Sol Aniel <129297452+Sol-Aniel@users.noreply.github.com>
2024-12-13 09:25:24 -0300

    Delete Projeto São Longuinho directory

--- Projeto São Longuinho/controllers/__init__.py
+++
@@ -1,1 +0,0 @@
-from .longController import longuinho
--- Projeto São Longuinho/controllers/longController.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint,render_template
-
-longuinho = Blueprint('longuinho',__name__)
-
-@longuinho.route('/')
-def index():
-    return f"Longuinho"
