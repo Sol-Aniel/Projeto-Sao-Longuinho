2c025f688d39e3ef378934d2372bcfa0ca9823a3
Sol Aniel <129297452+Sol-Aniel@users.noreply.github.com>
2024-12-16 00:57:52 -0300

    Delete Projeto-Sao-Longuinho directory

--- Projeto-Sao-Longuinho/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Objetos import Objetos
-from .Clientes import Clientes
-from .Funcionarios import Funcionarios
-from .Tipos import Tipos
-from .Administradores import Administradores
-from .Categorias import Categorias
-from .Equipes import Equipes
