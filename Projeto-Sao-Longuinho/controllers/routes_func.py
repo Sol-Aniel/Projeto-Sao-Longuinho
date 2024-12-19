282fe880153bfe039d5a7ace20ceccfe59b44bd8
Sol Aniel <129297452+Sol-Aniel@users.noreply.github.com>
2024-12-19 17:13:08 -0300

    Update routes_func.py

--- Projeto-Sao-Longuinho/controllers/routes_func.py
+++ Projeto-Sao-Longuinho/controllers/routes_func.py
@@ -31,7 +31,10 @@ def pendentes():
 @funcionarios.route('/achados')
 def achados():
     objetos_achados = objetos_rep.get_objetos_by('found', True)
-    return render_template('achados.html', objetos=objetos_achados)
+    categorias = categorias_rep.get_categorias()
+    clientes = clientes_rep.get_clientes()
+    equipes = equipes_rep.get_equipes()
+    return render_template('achados.html', clientes=clientes, equipes=equipes, categorias=categorias, objetos=objetos_achados)
 
 @funcionarios.route('/alterar-status/<int:obj_id>', methods=['POST'])
 def modstatus(obj_id):
